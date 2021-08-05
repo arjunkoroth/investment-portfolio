9495277265c7d81c6033896de197cf4fdb369912
DESKTOP-2K3PVFT\ARJUN KOROTH <arjunkoroth@hotmail.com>
2021-08-05 17:10:52 +0530

    User repo changes

--- customer-service/src/main/java/com/hackathon/customerservice/repository/UserRepository.java
+++ customer-service/src/main/java/com/hackathon/customerservice/repository/UserRepository.java
@@ -4,11 +4,11 @@ import com.hackathon.customerservice.entity.UserDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserDetail,Long> {
 	
-	public List<UserDetail> findByCustomerId(String customerId);
+	public Optional<UserDetail> findByCustomerId(String customerId);
 	
 }
