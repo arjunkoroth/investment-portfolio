7409a27d397079505c8a744c331c540612cf13bc
HindujaSekar <shinduja97@gmail.com>
2021-08-05 16:17:47 +0530

    added test case

--- customer-service/src/main/java/com/hackathon/customerservice/client/StockServiceProxy.java
+++ customer-service/src/main/java/com/hackathon/customerservice/client/StockServiceProxy.java
@@ -1,12 +1,17 @@
 package com.hackathon.customerservice.client;
 
+import com.hackathon.customerservice.dto.StockDTO;
+import javax.validation.constraints.NotNull;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @FeignClient(name = "http://STOCK-SERVICE/stock-service")
 public interface StockServiceProxy {
-    
-    @GetMapping
-    public double getStockPrice(String stockCode);
+
+    @GetMapping("/{stockCode}")
+    public StockDTO getStrockPrice(@PathVariable("stockCode") @NotNull Optional<String> stockCode);
 
 }
--- customer-service/src/test/java/com/hackathon/customerservice/service/UserServiceTest.java
+++ customer-service/src/test/java/com/hackathon/customerservice/service/UserServiceTest.java
@@ -1,5 +1,16 @@
 package com.hackathon.customerservice.service;
 
+import com.hackathon.customerservice.client.StockServiceProxy;
+import com.hackathon.customerservice.dto.OrderRequestDto;
+import com.hackathon.customerservice.dto.OrderResponseDto;
+import com.hackathon.customerservice.dto.StockDTO;
+import com.hackathon.customerservice.entity.InvestmentAccount;
+import com.hackathon.customerservice.entity.PortfolioDetail;
+import com.hackathon.customerservice.repository.InvestmentAccountRepository;
+import com.hackathon.customerservice.repository.OrderDetailRepository;
+import com.hackathon.customerservice.repository.PortfolioDetailRepository;
+import com.hackathon.customerservice.repository.UserRepository;
+import com.hackathon.customerservice.service.impl.UserServiceImpl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
@@ -21,12 +32,27 @@ import com.hackathon.customerservice.entity.UserRole;
 import com.hackathon.customerservice.exceptions.InvalidCredentialsException;
 import com.hackathon.customerservice.repository.UserRepository;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 
 @SpringBootTest
 class UserServiceTest {
 	
 	@Mock
     private UserRepository userRepository;
+    @Mock
+    private InvestmentAccountRepository investmentAccountRepository;
+    @Mock
+    private PortfolioDetailRepository portfolioDetailRepository;
+    @Mock
+    private StockServiceProxy stockServiceProxy;
+    @Mock
+    private OrderDetailRepository orderDetailRepository;
 
     @InjectMocks
     private UserService userService;
@@ -38,6 +64,17 @@ class UserServiceTest {
     }
     
     @Test
+    public void testOrderStock() {
+        when(investmentAccountRepository.findByAccountNumber(any(String.class)))
+                .thenReturn(Optional.of(new InvestmentAccount()));
+        when(stockServiceProxy.getStrockPrice(any()))
+                .thenReturn(StockDTO.builder().build());
+        when(portfolioDetailRepository.findByInvestementAccount(any(InvestmentAccount.class)))
+                .thenReturn(Optional.of(new PortfolioDetail()));
+        OrderResponseDto response = userService.orderstock(OrderRequestDto.builder().build(),"accountNumber");
+        assertNotNull(response);
+        assertEquals(response.getStatusCode(),201);
+
     void testLoginUsingCustomerId() {
     	UserRole userRole = UserRole.builder().id(1).roleName("ROLE_CUSTOMER").build();
     	UserDetail userDetail = UserDetail.builder()
